7d7596af3802f4bbd0988f0c209bb30d69774af9
Winter <25314591+AutumnClove@users.noreply.github.com>
2020-05-31 16:22:09 -0500

    Update fun.py
    Fixed extra lines

--- mods/fun.py
+++ mods/fun.py
@@ -158,13 +158,7 @@ class Fun(commands.Cog):
        def url_encode(word):
 	url = ""
 	for c in word:
-
 		if c not in self.filt:
-
-
-
-
-
 			url += "%" +  str(hex(ord(c))).replace("0x", "")
 		else:
 			url += c
